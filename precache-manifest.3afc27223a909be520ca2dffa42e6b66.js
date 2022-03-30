4e1d713317eca4d0863c2faf5c381d47a7c7a5f6
Maciej Grochowski <mghej3@wp.pl>
2022-03-30 19:17:04 +0200

    Updates

---
+++ precache-manifest.3afc27223a909be520ca2dffa42e6b66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ddd32f5973f3bcc607c9000916b8dcd",
+    "url": "/Users-Adder/index.html"
+  },
+  {
+    "revision": "8e8161763c38f7d803b3",
+    "url": "/Users-Adder/static/css/main.85c88ffb.chunk.css"
+  },
+  {
+    "revision": "e203b4f9770a8ed2aafb",
+    "url": "/Users-Adder/static/js/2.c2c5598b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Users-Adder/static/js/2.c2c5598b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8161763c38f7d803b3",
+    "url": "/Users-Adder/static/js/main.7bef52fd.chunk.js"
+  },
+  {
+    "revision": "2751522c794cec751477",
+    "url": "/Users-Adder/static/js/runtime-main.f433ce92.js"
+  }
+]);
